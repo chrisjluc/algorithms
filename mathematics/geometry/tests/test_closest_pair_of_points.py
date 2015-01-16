f323658290126fae3d2cdcd11c2f853cb7610b3d
chrisjluc <chrisluc@zanbato.com>
2015-01-15 23:03:40 -0800

    Added more tests for edge cases

--- mathematics/geometry/tests/test_closest_pair_of_points.py
+++ mathematics/geometry/tests/test_closest_pair_of_points.py
@@ -15,6 +15,19 @@ class TestClosestPairOfPoints(unittest.TestCase):
         self.assertTrue((0, 1) in actual)
         self.assertTrue((1, 1) in actual)
 
+    def test_vertical_points(self):
+        P = [(0, 1), (0, 7), (0, 0), (0, 4), (0, 9)]
+        actual = closest_pair_of_points.closest_pair_of_points(P)
+        self.assertTrue((0, 0) in actual)
+        self.assertTrue((0, 1) in actual)
+
+    def test_vertical_points(self):
+        P = [(1, 0), (7, 0), (0, 0), (4, 0), (9, 0)]
+        actual = closest_pair_of_points.closest_pair_of_points(P)
+        self.assertTrue((1, 0) in actual)
+        self.assertTrue((0, 0) in actual)
+
+
     def test_alot_of_points(self):
         P = [(5, 0), (0, 1), (2, 0), (1, 1), (-1, 3), (0, 4), (4, 6), (4, 4), (4, 8), (3, 3), (-1, 7)]
         actual = closest_pair_of_points.closest_pair_of_points(P)
